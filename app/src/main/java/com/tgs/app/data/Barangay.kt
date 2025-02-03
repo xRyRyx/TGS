afbe05cadf99136165cbeb31a2568f0e11296ced
xRyRyx <202101186@iacademy.edu.ph>
2025-02-03 17:34:44 +0800

    deletion of unused classes - mg

--- app/src/main/java/com/tgs/app/data/Barangay.kt
+++
@@ -1,6 +0,0 @@
-package com.tgs.app.data
-
-data class Barangay(
-    val id: String,
-    val name: String
-)
--- app/src/main/java/com/tgs/app/data/Province.kt
+++
@@ -1,6 +0,0 @@
-package com.tgs.app.data
-
-data class Province(
-    val id: String,
-    val name: String
-)
