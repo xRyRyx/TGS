f4eb10ea130c59dc84acb2e7b58ccf22c4a0ae05
xRyRyx <202101186@iacademy.edu.ph>
2025-02-07 09:49:49 +0800

    deletion of files

--- app/src/main/java/com/tgs/app/provinces/LocationModels.kt
+++
@@ -1,9 +0,0 @@
-package com.tgs.app.provinces
-
-class LocationModels {
-    data class CityMunicipality(val id: String, val name: String)
-
-    data class Barangay(val id: String, val name: String)
-
-    data class Province(val id: String, val name: String)
-}
